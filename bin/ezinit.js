83f8c04b64c6e4990db94624615bf2e409dcd2c9
Haril Song <songkg7@gmail.com>
2021-08-24 07:02:30 +0900

    feat: 1.1.0 complete

--- bin/ezinit.js
+++ bin/ezinit.js
@@ -4,8 +4,6 @@ const { program } = require('commander');
 const packageJson = require('../package.json');
 const execSync = require('child_process').execSync;
 const chalk = require('chalk');
-const path = require('path');
-const projectName = path.parse(__dirname).name;
 
 // let Progress = require('progress'),
 //   bar = new Progress('running [:bar] :percent :etas', {
@@ -18,22 +16,21 @@ const projectName = path.parse(__dirname).name;
 program.usage('[option]');
 
 program
-  .command('init')
+  .argument('<name>', 'Github repository name')
+  .argument('[description]', 'Repository description', 'created By EZinit')
   .version(packageJson.version)
-  .usage('[-b] <-n|--name> [-d|--description]')
+  .usage('[flags] <repository-name> [description]')
   .description('initialize git project to github')
-  .requiredOption('-n, --name <name>', 'Github repository name')
-  .option('-d, --description <description>', 'Repository description', 'created By EZinit')
   .option('-b, --browse', 'open browser', false)
-  .action((options) => {
+  .action((name, description) => {
     console.log(chalk.green('start') + " progress...");
     execSync('git init');
-    execSync(`echo "# ${options.name}" > README.md`);
+    execSync(`echo "# ${name}" > README.md`);
     execSync('git add README.md');
     execSync('git commit -m "first commit"', {
       timeout: 10000
     });
-    execSync(`gh repo create "${options.name}" -y -d "${options.description}" --public`, {
+    execSync(`gh repo create "${name}" -y -d "${description}" --public`, {
       timeout: 30000
     });
     execSync('git push -u origin master');
